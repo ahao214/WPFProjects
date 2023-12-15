3375131eafd40e247e4ddaa1d56067a6af2e266f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-15 20:08:19 +0800

    窗口关闭操作

--- CourseManagementSystem/CourseManager/ViewModel/LoginViewModel.cs
+++ CourseManagementSystem/CourseManager/ViewModel/LoginViewModel.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using CourseManager.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CourseManager.ViewModel
 {
     public class LoginViewModel
     {
+        /// <summary>
+        /// 关闭按钮事件
+        /// </summary>
+        public CommandBase CloseWindowCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            this.CloseWindowCommand = new CommandBase();
+            this.CloseWindowCommand.DoExecute = new Action<object>((o) =>
+            {
+                (o as Window).Close();
+            });
+
+            this.CloseWindowCommand.DoCanExecute = new Func<object, bool>((o) => { return true; });
+        }
+
+
     }
 }
