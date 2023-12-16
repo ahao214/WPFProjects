1d803a1b8986fd404bc194c8ad7178bdd17378a1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-16 12:02:19 +0800

    全局变量

---
+++ CourseManagementSystem/CourseManager/Common/GlobalValues.cs
@@ -0,0 +1,18 @@
+﻿using CourseManager.DataAccess.DataEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.Common
+{
+    /// <summary>
+    /// 全局变量
+    /// </summary>
+    public class GlobalValues
+    {
+        public static UserEntity UserInfo { get; set; }
+
+    }
+}
