f7b77bab14a5fcab7260d50dfee5d6eedf37429c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-16 19:41:48 +0800

    性别转换器

---
+++ CourseManagementSystem/CourseManager/Converter/GenderConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CourseManager.Converter
+{
+    /// <summary>
+    /// 性别转换器
+    /// </summary>
+    public class GenderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null || parameter == null)
+            {
+                return false;
+            }
+            return value.ToString() == parameter.ToString();
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return parameter;
+
+        }
+    }
+}
