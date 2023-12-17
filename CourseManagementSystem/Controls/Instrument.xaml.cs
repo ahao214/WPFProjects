91e72f481e3e126b399de4398b2174b2b8578242
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-17 19:16:43 +0800

    刻度绘制

--- CourseManagementSystem/Controls/Instrument.xaml.cs
+++ CourseManagementSystem/Controls/Instrument.xaml.cs
@@ -20,6 +20,29 @@ namespace Controls
     /// </summary>
     public partial class Instrument : UserControl
     {
+        // 依赖属性,依赖对象
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(Instrument), new PropertyMetadata(double.NaN, new PropertyChangedCallback(OnPropertyChanged)));
+
+        /// <summary>
+        /// Value发生变化，调用这里
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        public static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as Instrument).Refresh();
+        }
+
+
+
         public Instrument()
         {
             InitializeComponent();
@@ -32,6 +55,64 @@ namespace Controls
             double minSize = Math.Min(this.RenderSize.Width, this.RenderSize.Height);
             this.backEllipse.Width = minSize;
             this.backEllipse.Height = minSize;
+        }
+
+        private void Refresh()
+        {
+            double radius = this.backEllipse.Width / 2;
+
+            this.mainCanvas.Children.Clear();
+
+            double min = 0, max = 100;
+            double scaleAreaCount = 10.0;
+            double step = 270.0 / (max - min);
+
+            // 绘制小刻度
+            for (int i = 0; i < max - min; i++)
+            {
+                Line lineScale = new Line();
+                lineScale.X1 = radius - (radius - 13) * Math.Cos((i * step - 45) * Math.PI / 180);
+                lineScale.Y1 = radius - (radius - 13) * Math.Sin((i * step - 45) * Math.PI / 180);
+
+                lineScale.X2 = radius - (radius - 8) * Math.Cos((i * step - 45) * Math.PI / 180);
+                lineScale.Y2 = radius - (radius - 8) * Math.Sin((i * step - 45) * Math.PI / 180);
+
+                lineScale.Stroke = Brushes.White;
+                lineScale.StrokeThickness = 1;
+
+                this.mainCanvas.Children.Add(lineScale);
+            }
+
+            step = 270.0 / scaleAreaCount;
+            int scaleText = (int)min;
+            // 绘制大刻度
+            for (int i = 0; i <= scaleAreaCount; i++)
+            {
+                Line lineScale = new Line();
+                lineScale.X1 = radius - (radius - 20) * Math.Cos((i * step - 45) * Math.PI / 180);
+                lineScale.Y1 = radius - (radius - 20) * Math.Sin((i * step - 45) * Math.PI / 180);
+
+                lineScale.X2 = radius - (radius - 8) * Math.Cos((i * step - 45) * Math.PI / 180);
+                lineScale.Y2 = radius - (radius - 8) * Math.Sin((i * step - 45) * Math.PI / 180);
+
+                lineScale.Stroke = Brushes.White;
+                lineScale.StrokeThickness = 1;
+
+                this.mainCanvas.Children.Add(lineScale);
+
+                TextBlock textScale = new TextBlock();
+                textScale.Width = 34;
+                textScale.TextAlignment = TextAlignment.Center;
+                textScale.FontSize = 14;
+
+                textScale.Text = (scaleText + (max - min) / scaleAreaCount * i).ToString();
+                textScale.Foreground = Brushes.White;
+                Canvas.SetLeft(textScale, radius - (radius - 36) * Math.Cos((i * step - 45) * Math.PI / 180) - 17);
+                Canvas.SetTop(textScale, radius - (radius - 36) * Math.Sin((i * step - 45) * Math.PI / 180) - 10);
+
+                this.mainCanvas.Children.Add(textScale);
+            }
+
         }
     }
 }
