9366d2fe1a2461798033d2def70aa16a45d6030b
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-18 22:15:55 +0800

    获取数据库连接的Value

--- PackingLotSystem/Joker.SmartPacking.Server.Configuration/Configuration.cs
+++ PackingLotSystem/Joker.SmartPacking.Server.Configuration/Configuration.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace Joker.SmartPacking.Server.Configuration
 {
     public class Configuration : IConfiguration.IConfiguration
     {
+        private static IConfigurationRoot configurationRoot;
+
+        static Configuration()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            configurationRoot = builder.Build();
+
+        }
+
+        /// <summary>
+        /// 获取数据库连接的Value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public string Read(string key)
         {
-            throw new NotImplementedException();
-
+            return configurationRoot[key];
         }
     }
 }
