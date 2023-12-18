a742ada16959f2840311c95d7246a0e435a5f387
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-18 16:07:04 +0800

    CourseSeriesModel

---
+++ CourseManagementSystem/CourseManager/Model/CourseSeriesModel.cs
@@ -0,0 +1,25 @@
+﻿using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CourseSeriesModel
+    {
+        /// <summary>
+        /// 课程名称
+        /// </summary>
+        public string CourseName { get; set; }
+
+        public SeriesCollection SeriesColection { get; set; }
+
+        public ObservableCollection <SeriesModel> SeriesList { get; set; }
+    }
+}
---
+++ CourseManagementSystem/CourseManager/Model/SeriesModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.Model
+{
+    public class SeriesModel
+    {
+        public string SeriesName { get; set; }
+
+        public int CurrentValue { get; set; }
+
+        public bool IsGrowing { get; set; }
+
+        public int ChangeRate { get; set; }
+
+    }
+}
