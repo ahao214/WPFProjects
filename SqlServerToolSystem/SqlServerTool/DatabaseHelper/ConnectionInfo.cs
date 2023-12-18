385adad6d54cc41c67af0b4404addd696c0cc960
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-18 12:36:10 +0800

    add 数据库模型

---
+++ SqlServerToolSystem/SqlServerTool/DatabaseHelper/ConnectionInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerTool.DatabaseHelper
+{
+    /// <summary>
+    /// 数据库模型
+    /// </summary>
+    public class ConnectionInfo
+    {
+        /// <summary>
+        /// 数据库服务器地址(名称)
+        /// </summary>
+        public string Server { get; set; }
+        /// <summary>
+        /// 数据库登录ID
+        /// </summary>
+        public string UserId { get; set; }
+        /// <summary>
+        /// 数据库登录密码
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// 数据库的名称
+        /// </summary>
+        public string DataBase {  get; set; }
+
+    }
+}
--- SqlServerToolSystem/SqlServerTool/MainWindow.xaml.cs
+++ SqlServerToolSystem/SqlServerTool/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace SqlServerTool
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region 构造函数
+        /// <summary>
+        /// 构造函数
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region 开始连接数据库事件
+        /// <summary>
+        /// 开始连接数据库事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnConn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 退出程序
+        /// <summary>
+        /// 退出程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            //Close();    
+            //Application.Current.Shutdown();
+            Environment.Exit(0);
+        }
+        #endregion
     }
 }
