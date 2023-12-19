cd51a15c3365f1159544265165045857c05f8407
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-19 15:26:10 +0800

    add LoginViewModel

---
+++ PackingLotSystem/Joker.SmartPacking.Client.Start/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joker.SmartPacking.Client.Start.ViewModels
+{
+    public class LoginViewModel
+    {
+
+    }
+}
