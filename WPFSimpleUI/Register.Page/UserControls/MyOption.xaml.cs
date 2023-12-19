5c099e140e44aa2de9b578ae6599342bb6287789
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-19 23:03:06 +0800

    用户控件

--- WPFSimpleUI/Register.Page/UserControls/MyOption.xaml.cs
+++ WPFSimpleUI/Register.Page/UserControls/MyOption.xaml.cs
@@ -24,5 +24,30 @@ namespace Register.Page.UserControls
         {
             InitializeComponent();
         }
+
+
+
+
+        public string  Text
+        {
+            get { return (string )GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string ), typeof(MyOption));
+
+
+        public FontAwesome.WPF.FontAwesomeIcon Icon
+        {
+            get { return (FontAwesome.WPF.FontAwesomeIcon)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(FontAwesome.WPF.FontAwesomeIcon), typeof(MyOption));
+
     }
 }
