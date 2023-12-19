3eedb76ea3caf79c0a155a1b14405e01c5da4b26
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-19 14:19:22 +0800

    MD5 加密

--- PackingLotSystem/Joker.SmartPacking.Server.Start/Controllers/UserController.cs
+++ PackingLotSystem/Joker.SmartPacking.Server.Start/Controllers/UserController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Joker.SmartPacking.Server.IService;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Joker.SmartPacking.Server.Start.Controllers
 {
@@ -7,13 +11,38 @@ namespace Joker.SmartPacking.Server.Start.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        ILoginService _loginService;
+        public UserController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
         [HttpPost]
         [Route("login")]
-        public string Login([FromForm]string username,[FromForm]string password)
+        public string Login([FromForm] string username, [FromForm] string password)
         {
+
             return "kuteng";
         }
 
+        #region MD5 加密
+        /// <summary>
+        /// MD5 加密
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private string GetMd5Str(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return "";
+            }
+            byte[] result = Encoding.Default.GetBytes(str);
+            MD5 md = new MD5CryptoServiceProvider();
+            byte[] output = md.ComputeHash(result);
+            return BitConverter.ToString(output).Replace("-", "");
 
+        } 
+        #endregion
     }
 }
