76a95efe9f635493ad5abb196a2331cbba25d35d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-19 22:54:51 +0800

    用户控件

--- WPFSimpleUI/Register.Page/MainWindow.xaml.cs
+++ WPFSimpleUI/Register.Page/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Register.Page
 {
@@ -19,6 +12,14 @@ namespace Register.Page
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton ==  MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
     }
 }
---
+++ WPFSimpleUI/Register.Page/UserControls/MyTextBox.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Register.Page.UserControls
+{
+    /// <summary>
+    /// MyTextBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyTextBox : UserControl
+    {
+        public MyTextBox()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public string Hint
+        {
+            get { return (string)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+       
+        public static readonly DependencyProperty HintProperty =
+            DependencyProperty.Register("Hint", typeof(string), typeof(MyTextBox));
+
+
+
+
+    }
+}
