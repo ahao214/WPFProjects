0864e68ebe65a3031a63177e77aeccbf1dea6f3a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-19 20:26:23 +0800

    业务逻辑层

---
+++ PackingLotSystem/Joker.SmartPacking.Client.BLL/LoginBll.cs
@@ -0,0 +1,23 @@
+﻿using Joker.SmartPacking.Client.IBLL;
+using Joker.SmartPacking.Client.IDAL;
+using System;
+using System.Threading.Tasks;
+
+namespace Joker.SmartPacking.Client.BLL
+{
+    public class LoginBll : ILoginBll
+    {
+        ILoginDal _loginDal;
+        public LoginBll(ILoginDal loginDal)
+        {
+            _loginDal = loginDal;
+        }
+
+        public async Task<bool> Login(string username, string password)
+        {
+            var loginStr = await _loginDal.Login(username, password);
+
+            return false;
+        }
+    }
+}
---
+++ PackingLotSystem/Joker.SmartPacking.Client.IBLL/ILoginBll.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Joker.SmartPacking.Client.IBLL
+{
+    public interface ILoginBll
+    {
+        Task<bool> Login(string username, string password);
+    }
+}
