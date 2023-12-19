9fbcc8feb596e57bdcb6c28d2d5df9315846fdfa
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-19 20:28:55 +0800

    add Entity

---
+++ PackingLotSystem/Joker.SmartPacking.Client.Entity/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Joker.SmartPacking.Client.Entity
+{
+    public class UserEntity
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string UserIcon { get; set; }    
+
+    }
+}
