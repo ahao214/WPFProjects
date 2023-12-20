148049514fb790b12bda49f5792d6a19436890f1
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-20 16:23:51 +0800

    sys_user_info表结构的修改

--- PackingLotSystem/Joker.SmartPacking.Server.Models/SysUserInfo.cs
+++ PackingLotSystem/Joker.SmartPacking.Server.Models/SysUserInfo.cs
@@ -12,13 +12,14 @@ namespace Joker.SmartPacking.Server.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("_id")]
+        [Column("user_id")]
         public int Id { get; set; }
         [Column("user_name")]
         public string UserName { get; set; }
         [Column("password")]
-        public string Password { get; set; }    
-
+        public string Password { get; set; }
+        [Column("user_icon")]
+        public string UserIcon { get; set; }
 
     }
 
