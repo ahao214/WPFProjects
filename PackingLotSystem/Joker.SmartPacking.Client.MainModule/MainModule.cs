f066c2e9a0e7474c438e1e22b1dcbab675e2d8a7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-20 19:36:05 +0800

    MainModule

---
+++ PackingLotSystem/Joker.SmartPacking.Client.MainModule/MainModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Joker.SmartPacking.Client.MainModule
+{
+    public class MainModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
