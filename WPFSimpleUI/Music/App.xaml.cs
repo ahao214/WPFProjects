d88ab0d524d99496ce8ee5178eb112b59dadcfa8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-21 15:33:56 +0800

    音乐文本框

---
+++ WPFSimpleUI/Music/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Music
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
