5b63e36145182b134cd1c0295600bc90eb7e7321
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-21 12:47:05 +0800

    菜单

---
+++ PackingLotSystem/Joker.SmartPacking.Client.Entity/MenuEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joker.SmartPacking.Client.Entity
+{
+    /// <summary>
+    /// 菜单
+    /// </summary>
+    public class MenuEntity
+    {
+        public int MenuId { get; set; }
+
+        public string MenuHeader { get; set; }
+
+        public string TargetView { get; set; }
+
+        public int ParendId { get; set; }
+
+        public string MenuIcon { get; set; }
+
+        public int Index { get; set; }
+
+        public int MenuType { get; set; }
+
+    }
+}
