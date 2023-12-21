c3becc80c87315bec57bf8977f03a26831044f3f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-21 12:47:51 +0800

    全局Entity

---
+++ PackingLotSystem/Joker.SmartPacking.Client.Entity/GlobalEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joker.SmartPacking.Client.Entity
+{
+    /// <summary>
+    /// 全局Entity
+    /// </summary>
+    public class GlobalEntity
+    {
+    }
+}
