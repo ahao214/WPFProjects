02f1f22510cbb55397e2f85b0610c8585e1efde4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-22 12:39:08 +0800

    查看表结构SQL语句

--- SqlServerToolSystem/SqlServerTool/SqlItem/SqlConst.cs
+++ SqlServerToolSystem/SqlServerTool/SqlItem/SqlConst.cs
@@ -19,5 +19,13 @@ namespace SqlServerTool.SqlItem
         public const string GetTables = "SELECT id,name FROM sysobjects WHERE xtype='{0}' AND id > 0 AND name <> 'dtproperties' ORDER BY name";
 
         #endregion
+
+        #region 查看表结构SQL语句
+        /// <summary>
+        /// 查看表结构SQL语句
+        /// </summary>
+        public const string GetColumns = @"SELECT c.name as Name,t.name as DataType,c.max_length as MaxLength,c.is_nullable as IsNullable FROM SYSOBJECTS AS o inner join sys.columns AS c ON c.object_id=o.id inner join sys.types AS t ON t.system_type_id=c.system_type_id WHERE o.name='{0}' ORDER BY c.name";
+
+        #endregion
     }
 }
