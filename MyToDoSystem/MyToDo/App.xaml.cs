7ae16fbf3bc182024542c10b04ac504a0cc89ad0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-27 23:11:50 +0800

    代办事项系统

---
+++ MyToDoSystem/MyToDo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyToDo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
