0bcc2981a02275caf0b828d89d5d3fc3a2aa4491
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-28 13:58:49 +0800

    首页样式

--- MyToDoSystem/MyToDo/ViewModels/IndexViewModel.cs
+++ MyToDoSystem/MyToDo/ViewModels/IndexViewModel.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using MyToDo.Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Configuration;
 
 namespace MyToDo.ViewModels
 {
-    public class IndexViewModel
+    public class IndexViewModel:BindableBase
     {
+
+        public IndexViewModel()
+        {
+            TaskBars = new ObservableCollection<TaskBar>();
+            CreateTaskBars();
+        }
+
+        private ObservableCollection<TaskBar> taskBars;
+
+        public ObservableCollection<TaskBar> TaskBars
+        {
+            get { return taskBars; }
+            set { taskBars = value; RaisePropertyChanged(); }
+        }
+
+        void CreateTaskBars()
+        {
+            TaskBars.Add(new TaskBar { Icon = "ClockFast", Title = "汇总", Content = "9", Color = "#FF0CA0FF", Target = "" });
+            TaskBars.Add(new TaskBar { Icon = "ClockCheckOutline", Title = "已完成", Content = "9", Color = "#FF1ECA3A", Target = "" });
+            TaskBars.Add(new TaskBar { Icon = "ChartLineVariant", Title = "完成比例", Content = "100%", Color = "#FF02C6DC", Target = "" });
+            TaskBars.Add(new TaskBar { Icon = "PlaylistStar", Title = "备忘录", Content = "19", Color = "#FFFFA000", Target = "" });
+
+        }
 
 
     }
