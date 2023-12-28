c1c9750f61fee55de8f10903a0389c702e466955
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-28 14:01:19 +0800

    add ToDoDto

---
+++ MyToDoSystem/MyToDo/Common/Models/ToDoDto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.Common.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ToDoDto
+    {
+
+        private int id;
+        private string title;
+        private string content;
+        private int status;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+
+        public int Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+
+
+    }
+}
