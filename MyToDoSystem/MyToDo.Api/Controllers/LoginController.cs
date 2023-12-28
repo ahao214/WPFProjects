f64c06e272f351ecac598cfa375f164017ae409a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-28 20:19:05 +0800

    LoginController

---
+++ MyToDoSystem/MyToDo.Api/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyToDo.Api.Service;
+using MyToDo.Shared.Dtos;
+
+namespace MyToDo.Api.Controllers
+{
+    /// <summary>
+    /// 账户控制器
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService service;
+
+        public LoginController(ILoginService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost]
+        public async Task<ApiResponse> Login([FromBody] UserDto param) =>
+            await service.LoginAsync(param.Account, param.PassWord);
+
+        [HttpPost]
+        public async Task<ApiResponse> Resgiter([FromBody] UserDto param) =>
+            await service.Resgiter(param);
+    }
+}
