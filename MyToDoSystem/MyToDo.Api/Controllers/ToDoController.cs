fb56435c59668dbba2340db83adca94aff317746
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-28 19:50:42 +0800

    待办事项控制器

--- MyToDoSystem/MyToDo.Api/Controllers/ToDoController.cs
+++ MyToDoSystem/MyToDo.Api/Controllers/ToDoController.cs
@@ -11,17 +11,27 @@ namespace MyToDo.Api.Controllers
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class ToDoController : ControllerBase
-    {        
+    {
         private readonly IToDoService _service;
 
         public ToDoController(IToDoService service)
-        {            
+        {
             _service = service;
         }
 
         [HttpGet]
-        public async Task<ApiResponse> Get(int id) =>await _service.GetSingleAsync(id);       
+        public async Task<ApiResponse> Get(int id) => await _service.GetSingleAsync(id);
 
+        //[HttpGet]
+        //public async Task<ApiResponse> GetAll(int id) => await _service.GetAllAsync(id);
 
+        [HttpPost]
+        public async Task<ApiResponse> Add([FromBody] ToDo model) => await _service.AddAsync(model);
+
+        [HttpPost]
+        public async Task<ApiResponse> Update([FromBody] ToDo model) => await _service.UpdateAsync(model);
+
+        [HttpDelete]
+        public async Task<ApiResponse> Delete(int id) => await _service.DeleteAsync(id);
     }
 }
