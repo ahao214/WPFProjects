54d9361669ffa8aa9de806acd48e40b03842cc12
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-28 15:06:04 +0800

    备忘录页面

--- MyToDoSystem/MyToDo/ViewModels/MemoViewModel.cs
+++ MyToDoSystem/MyToDo/ViewModels/MemoViewModel.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MyToDo.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
 
 namespace MyToDo.ViewModels
 {
-    public class MemoViewModel
+    public class MemoViewModel: BindableBase
     {
+        public MemoViewModel()
+        {
+            MemoDtos = new ObservableCollection<MemoDto>();
+            CreateToDoList();
+            AddCommand = new DelegateCommand(Add);
+        }
+        private bool isRightDrawerOpen;
+        /// <summary>
+        /// 右侧编辑窗口是否展开
+        /// </summary>
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// 添加待办事项
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        private void Add()
+        {
+            IsRightDrawerOpen = true;
+        }
+
+        public DelegateCommand AddCommand { get; private set; }
+
+        private ObservableCollection<MemoDto> memoDtos;
+
+        public ObservableCollection<MemoDto> MemoDtos
+        {
+            get { return memoDtos; }
+            set { memoDtos = value; RaisePropertyChanged(); }
+        }
+
+
+        void CreateToDoList()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                MemoDtos.Add(new MemoDto
+                {
+                    Title = "标题" + i,
+                    Content = "内容" + i
+                });
+            }
+        }
     }
 }
