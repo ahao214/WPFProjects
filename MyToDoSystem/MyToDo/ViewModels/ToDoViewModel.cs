3433e6a2f60a26d09dcdc9f1d9bc0282ba68435a
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-28 23:37:10 +0800

    todoview

--- MyToDoSystem/MyToDo/ViewModels/ToDoViewModel.cs
+++ MyToDoSystem/MyToDo/ViewModels/ToDoViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using MyToDo.Shared.Dtos;
 using Prism.Ioc;
 using Prism.Regions;
+using System.Windows.Controls.Primitives;
 
 namespace MyToDo.ViewModels
 {
@@ -13,8 +14,19 @@ namespace MyToDo.ViewModels
         public ToDoViewModel(IToDoService service, IContainerProvider containerProvider) : base(containerProvider)
         {
             ToDoDtos = new ObservableCollection<ToDoDto>();
-            AddCommand = new DelegateCommand(Add);
+            ExecuteCommand = new DelegateCommand<string>(Execute);
+            SelectedCommand = new DelegateCommand<ToDoDto>(Selected);
             _service = service;
+        }
+
+        private void Execute(string obj)
+        {
+            switch (obj)
+            {
+                case "新增": Add(); break;
+                case "查询": GetDataAsync(); break;
+                case "保存": Save(); break;
+            }
         }
 
         private bool isRightDrawerOpen;
@@ -36,8 +48,54 @@ namespace MyToDo.ViewModels
             IsRightDrawerOpen = true;
         }
 
-        public DelegateCommand AddCommand { get; private set; }
+        private async void Save()
+        {
+            if (string.IsNullOrWhiteSpace(CurrentDto.Title) ||
+                string.IsNullOrWhiteSpace(CurrentDto.Content))
+                return;
 
+            UpdateLoading(true);
+
+            try
+            {
+                if (CurrentDto.Id > 0)
+                {
+                    var updateResult = await _service.UpdateAsync(CurrentDto);
+                    if (updateResult.Status)
+                    {
+                        var todo = ToDoDtos.FirstOrDefault(t => t.Id == CurrentDto.Id);
+                        if (todo != null)
+                        {
+                            todo.Title = CurrentDto.Title;
+                            todo.Content = CurrentDto.Content;
+                            todo.Status = CurrentDto.Status;
+                        }
+                    }
+                    IsRightDrawerOpen = false;
+                }
+                else
+                {
+                    var addResult = await _service.AddAsync(CurrentDto);
+                    if (addResult.Status)
+                    {
+                        ToDoDtos.Add(addResult.Result);
+                        IsRightDrawerOpen = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                UpdateLoading(false);
+            }
+        }
+
+        public DelegateCommand<string> ExecuteCommand { get; private set; }
+        public DelegateCommand<ToDoDto> SelectedCommand { get; private set; }
+        public DelegateCommand<ToDoDto> DeleteCommand { get; private set; }
         private ObservableCollection<ToDoDto> toDoDtos;
         private readonly IToDoService _service;
 
@@ -46,6 +104,51 @@ namespace MyToDo.ViewModels
         {
             get { return toDoDtos; }
             set { toDoDtos = value; RaisePropertyChanged(); }
+        }
+
+        private string search;
+        /// <summary>
+        /// 搜索条件
+        /// </summary>
+        public string Search
+        {
+            get { return search; }
+            set { search = value; }
+        }
+
+
+
+        private ToDoDto currentDto;
+
+        /// <summary>
+        /// 编辑选中/新增时对象
+        /// </summary>
+        public ToDoDto CurrentDto
+        {
+            get { return currentDto; }
+            set { currentDto = value; RaisePropertyChanged(); }
+        }
+
+        private async void Selected(ToDoDto obj)
+        {
+            try
+            {
+                UpdateLoading(true);
+                var todoResult = await _service.GetFirstOrDefaultAsync(obj.Id);
+                if (todoResult.Status)
+                {
+                    CurrentDto = todoResult.Result;
+                    IsRightDrawerOpen = true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                UpdateLoading(false);
+            }
         }
 
         /// <summary>
@@ -58,7 +161,8 @@ namespace MyToDo.ViewModels
             var todoResult = await _service.GetAllAsync(new Shared.Parameters.QueryParameter()
             {
                 PageIndex = 0,
-                PageSize = 100
+                PageSize = 100,
+                Search = Search
             });
 
             if (todoResult.Status)
