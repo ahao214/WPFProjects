b5cd87b812d3047d0a4d934137751091b0e249b0
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-28 16:54:37 +0800

    单元

---
+++ MyToDoSystem/MyToDo.Api/Repository/IToDoRepository.cs
@@ -0,0 +1,36 @@
+﻿using MyToDo.Api.Context;
+
+namespace MyToDo.Api.Repository
+{
+    public interface IToDoRepository
+    {
+        Task<bool> Add(ToDo toDo);
+        Task<bool> Update(ToDo toDo);
+        Task<bool> Delete(int id);
+    }
+
+    public class ToDoRepository : IToDoRepository
+    {
+        private readonly MyToDoContext _context;
+
+        public ToDoRepository(MyToDoContext context)
+        {
+            _context = context;
+        }
+
+        public Task<bool> Add(ToDo toDo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(ToDo toDo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
