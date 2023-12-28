eb60f72281eb6b96d8789fdbf2897d296f126552
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-28 19:14:38 +0800

    控制器

---
+++ MyToDoSystem/MyToDo.Api/Controllers/ToDoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyToDo.Api.Context;
+
+namespace MyToDo.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ToDoController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> Get(int id)
+        {
+            var repository = _unitOfWork.GetRepository<ToDo>();
+
+
+            return null;
+        }
+
+
+    }
+}
