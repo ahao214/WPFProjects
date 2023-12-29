c1d7cddfebf5d246549a7b95742909522844b4af
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-29 15:04:35 +0800

    ToDo对话框

---
+++ MyToDoSystem/MyToDo/ViewModels/Dialog/AddToDoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.ViewModels.Dialog
+{
+    /// <summary>
+    /// ToDo对话框
+    /// </summary>
+    public class AddToDoViewModel
+    {
+
+    }
+}
