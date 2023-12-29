19e16fa8bea6949fe577c54fee3fcdb8d69ebb13
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-29 16:04:44 +0800

    DialogExtensions

--- MyToDoSystem/MyToDo/Extensions/DialogExtensions.cs
+++ MyToDoSystem/MyToDo/Extensions/DialogExtensions.cs
@@ -1,15 +1,33 @@
-﻿using MyToDo.Common.Events;
+﻿using MyToDo.Common;
+using MyToDo.Common.Events;
 using Prism.Events;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Prism.Services.Dialogs;
+
 
 namespace MyToDo.Extensions
 {
     public static class DialogExtensions
     {
+        /// <summary>
+        /// 询问窗口
+        /// </summary>
+        /// <param name="dialogHost">指定的DialogHost会话主机</param>
+        /// <param name="title">标题</param>
+        /// <param name="content">询问内容</param>
+        /// <param name="dialogHostName">会话主机名称(唯一)</param>
+        /// <returns></returns>
+        public static async Task<IDialogResult> Question(this IDialogHostService dialogHost,
+            string title, string content, string dialogHostName = "Root"
+            )
+        {
+            DialogParameters param = new DialogParameters();
+            param.Add("Title", title);
+            param.Add("Content", content);
+            param.Add("dialogHostName", dialogHostName);
+            var dialogResult = await dialogHost.ShowDialog("MsgView", param, dialogHostName);
+            return dialogResult;
+        }
+
         /// <summary>
         /// 推送等待消息
         /// </summary>
@@ -28,6 +46,37 @@ namespace MyToDo.Extensions
         public static void Register(this IEventAggregator aggregator, Action<UpdateModel> action)
         {
             aggregator.GetEvent<UpdateLoadingEvent>().Subscribe(action);
+        }
+
+
+
+        /// <summary>
+        /// 注册提示消息 
+        /// </summary>
+        /// <param name="aggregator"></param>
+        /// <param name="action"></param>
+        public static void ResgiterMessage(this IEventAggregator aggregator,
+            Action<MessageModel> action, string filterName = "Main")
+        {
+            aggregator.GetEvent<MessageEvent>().Subscribe(action,
+                ThreadOption.PublisherThread, true, (m) =>
+                {
+                    return m.Filter.Equals(filterName);
+                });
+        }
+
+        /// <summary>
+        /// 发送提示消息
+        /// </summary>
+        /// <param name="aggregator"></param>
+        /// <param name="message"></param>
+        public static void SendMessage(this IEventAggregator aggregator, string message, string filterName = "Main")
+        {
+            aggregator.GetEvent<MessageEvent>().Publish(new MessageModel()
+            {
+                Filter = filterName,
+                Message = message,
+            });
         }
     }
 }
