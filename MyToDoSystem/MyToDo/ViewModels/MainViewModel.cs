2b72d0a5dc4f21914e2254fe69234383b599d09c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-29 14:54:49 +0800

    IConfigureService

--- MyToDoSystem/MyToDo/ViewModels/MainViewModel.cs
+++ MyToDoSystem/MyToDo/ViewModels/MainViewModel.cs
@@ -3,12 +3,8 @@ using MyToDo.Extensions;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MyToDo.ViewModels
 {
