5e38ca9089a5806a3c5017d3480de0428992f797
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-29 15:05:17 +0800

    Memo对话框

---
+++ MyToDoSystem/MyToDo/ViewModels/Dialog/AdddMemoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.ViewModels.Dialog
+{
+    /// <summary>
+    /// Memo对话框
+    /// </summary>
+    public class AdddMemoViewModel
+    {
+    }
+}
