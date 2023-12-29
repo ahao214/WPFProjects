373daba3d0a4807d30597cd393ef787e22f3ebfd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-29 21:13:35 +0800

    LoginViewModel

--- MyToDoSystem/MyToDo/ViewModels/LoginViewModel.cs
+++ MyToDoSystem/MyToDo/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Services.Dialogs;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,13 @@ namespace MyToDo.ViewModels
 {
     public class LoginViewModel : BindableBase, IDialogAware
     {
+        public LoginViewModel()
+        {
+            ExecuteCommand = new DelegateCommand<string>(Execute);
+        }
+
+
+
         public string Title { get; set; } = "ToDo";
 
         public event Action<IDialogResult> RequestClose;
@@ -28,5 +36,52 @@ namespace MyToDo.ViewModels
         {
             
         }
+
+        public DelegateCommand<string> ExecuteCommand { get; private set; }
+
+
+        private string account;
+
+        public string Account
+        {
+            get { return account; }
+            set { account = value; }
+        }
+
+        private string  password;
+
+        public string  Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+
+        private void Execute(string obj)
+        {
+            switch (obj)
+            {
+                case "Login": Login();break;
+                case "LoginOut":LoginOut();break;
+
+            }
+        }
+
+        #region 登录方法
+
+        private void LoginOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region 退出方法
+
+        private void Login()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
