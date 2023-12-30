f86f84045d46889da383375ee4fe50ec168e3523
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-30 23:46:36 +0800

    PrismApplication

--- ShellSystem/Shell/App.xaml.cs
+++ ShellSystem/Shell/App.xaml.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Prism.Ioc;
+using Prism.Unity;
 using System.Windows;
+using Shell.Views;
 
 namespace Shell
 {
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            MainWindow window = new MainWindow();
+            return window;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
     }
 }
