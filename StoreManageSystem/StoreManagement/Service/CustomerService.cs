70cb67f0a1b453d38d4c906e6fde0ed6309706d7
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-01 23:32:00 +0800

    CustomerService

--- StoreManageSystem/StoreManagement/Service/CustomerService.cs
+++ StoreManageSystem/StoreManagement/Service/CustomerService.cs
@@ -6,36 +6,60 @@ using System.Threading.Tasks;
 
 namespace StoreManagement.Service
 {
+    /// <summary>
+    /// 客户
+    /// </summary>
     public class CustomerService : IService<Customer>
     {
         public int Delete(Customer t)
         {
-            throw new NotImplementedException();
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                db.Entry(t).State = System.Data.Entity.EntityState.Deleted;
+                return db.SaveChanges();
+            }
         }
 
         public int Insert(Customer t)
         {
-            throw new NotImplementedException();
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                db.Entry(t).State = System.Data.Entity.EntityState.Added;
+                return db.SaveChanges();
+            }
         }
 
         public Customer Select(int id)
         {
-            throw new NotImplementedException();
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                return db.Customer.FirstOrDefault(item => item.Id == id);
+            }
         }
 
         public Customer Select(string Name)
         {
-            throw new NotImplementedException();
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                return db.Customer.FirstOrDefault(item => item.Name == Name);
+            }
         }
 
         public List<Customer> Select()
         {
-            throw new NotImplementedException();
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                return db.Customer.ToList();
+            }
         }
 
         public int Update(Customer t)
         {
-            throw new NotImplementedException();
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                db.Entry(t).State = System.Data.Entity.EntityState.Modified;
+                return db.SaveChanges();
+            }
         }
     }
 }
