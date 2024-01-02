8e82eda3f96094e04e85202e2d8f332d9927e239
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-02 20:13:24 +0800

    商品类型

--- StoreManageSystem/StoreManagement/Service/GoodsTypeService.cs
+++ StoreManageSystem/StoreManagement/Service/GoodsTypeService.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace StoreManagement.Service
 {
-    public class GoodsTypeService
+    /// <summary>
+    /// 商品类型
+    /// </summary>
+    public class GoodsTypeService:IService<GoodsType>
     {
+        public int Delete(GoodsType t)
+        {
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                db.Entry(t).State = System.Data.Entity.EntityState.Deleted;
+                return db.SaveChanges();
+            }
+        }
+
+        public int Insert(GoodsType t)
+        {
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                db.Entry(t).State = System.Data.Entity.EntityState.Added;
+                return db.SaveChanges();
+            }
+        }
+
+        public GoodsType Select(int id)
+        {
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                return db.GoodsType.FirstOrDefault(item => item.Id == id);
+            }
+        }
+
+        public GoodsType Select(string Name)
+        {
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                return db.GoodsType.FirstOrDefault(item => item.Name == Name);
+            }
+        }
+
+        public List<GoodsType> Select()
+        {
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                return db.GoodsType.ToList();
+            }
+        }
+
+        public int Update(GoodsType t)
+        {
+            using (StoreDBEntities db = new StoreDBEntities())
+            {
+                db.Entry(t).State = System.Data.Entity.EntityState.Modified;
+                return db.SaveChanges();
+            }
+        }
     }
 }
