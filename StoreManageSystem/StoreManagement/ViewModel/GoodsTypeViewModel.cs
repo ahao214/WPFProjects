52dd4ee6e57102775c637d97798fd6f1c27cb04f
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-03 23:44:58 +0800

    物资设置

--- StoreManageSystem/StoreManagement/ViewModel/GoodsTypeViewModel.cs
+++ StoreManageSystem/StoreManagement/ViewModel/GoodsTypeViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using StoreManagement.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI;
 
 namespace StoreManagement.ViewModel
 {
@@ -12,5 +15,32 @@ namespace StoreManagement.ViewModel
     /// </summary>
     public class GoodsTypeViewModel:ViewModelBase
     {
+        public GoodsTypeViewModel()
+        {
+            GoodsTypeList = new GoodsTypeService().Select();
+        }
+
+        private GoodsType goodsType = new GoodsType();
+        public GoodsType GoodsType
+        {
+            get { return goodsType; }
+            set { goodsType = value; RaisePropertyChanged(); }
+        }
+
+        private List<GoodsType> goodsTypeList = new List<GoodsType>();
+        public List<GoodsType> GoodsTypeList
+        {
+            get { return goodsTypeList; }
+            set { goodsTypeList = value; RaisePropertyChanged(); }
+        }
+
+        //增加
+        public RelayCommand<UserControl> AddCommand
+        {
+            get
+            {
+
+            }
+        }
     }
 }
