e80033fdbdc49d8fe30a6f29900fc48c4b70d0ba
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-03 21:02:45 +0800

    首页九宫格显示

--- StoreManageSystem/StoreManagement/View/IndexView.xaml.cs
+++ StoreManageSystem/StoreManagement/View/IndexView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using StoreManagement.View;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,23 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace StoreManagement
+namespace StoreManagement.View
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// IndexView.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class IndexView : UserControl
     {
-        public MainWindow()
+        public IndexView()
         {
             InitializeComponent();
-
-            Loaded += (s, e) =>
-            {
-                container.Content = new IndexView();
-
-            };
-
         }
     }
 }
