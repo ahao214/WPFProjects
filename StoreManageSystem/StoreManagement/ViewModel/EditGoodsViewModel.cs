e902fa959ee8abfeb86d4eff675b65b8a67ab453
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-05 00:01:13 +0800

    物资登记

--- StoreManageSystem/StoreManagement/ViewModel/EditGoodsViewModel.cs
+++ StoreManageSystem/StoreManagement/ViewModel/EditGoodsViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using StoreManagement.Service;
+using System.Windows;
 
 
 namespace StoreManagement.ViewModel
@@ -16,53 +19,52 @@ namespace StoreManagement.ViewModel
             set { goods = value; }
         }
 
-        ///// <summary>
-        ///// 修改
-        ///// </summary>
-        //public RelayCommand<Window> EditCommand
-        //{
-        //    get
-        //    {
-        //        var command = new RelayCommand<Window>((window) =>
-        //        {
-        //            if (string.IsNullOrEmpty(Supplier.Name) == true ||
-        //            string.IsNullOrEmpty(Supplier.Contact) == true ||
-        //            string.IsNullOrEmpty(Supplier.Telephone) == true)
-        //            {
-        //                MessageBox.Show("用户名不能为空");
-        //                return;
-        //            }
+        /// <summary>
+        /// 修改
+        /// </summary>
+        public RelayCommand<Window> EditCommand
+        {
+            get
+            {
+                var command = new RelayCommand<Window>((window) =>
+                {
+                    if (string.IsNullOrEmpty(Goods.Name) == true ||                    
+                    string.IsNullOrEmpty(Goods.Serial) == true)
+                    {
+                        MessageBox.Show("序号和名称不能为空");
+                        return;
+                    }
 
-        //            var service = new SupplierService();
-        //            int count = service.Update(Supplier);
-        //            if (count > 0)
-        //            {
-        //                MessageBox.Show("修改成功");
-        //                // 关闭当前窗体
-        //                window.Close();
-        //            }
-        //            else
-        //            {
-        //                MessageBox.Show("修改失败");
-        //            }
-        //        });
-        //        return command;
-        //    }
-        //}
+                    var service = new GoodsService();
+                    int count = service.Update(Goods);
+                    if (count > 0)
+                    {
+                        MessageBox.Show("修改成功");
+                        // 关闭当前窗体
+                        window.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("修改失败");
+                    }
+                });
+                return command;
+            }
+        }
 
-        ///// <summary>
-        ///// 取消
-        ///// </summary>
-        //public RelayCommand<Window> CancelCommand
-        //{
-        //    get
-        //    {
-        //        var command = new RelayCommand<Window>((window) =>
-        //        {
-        //            window.Close();
-        //        });
-        //        return command;
-        //    }
-        //}
+        /// <summary>
+        /// 取消
+        /// </summary>
+        public RelayCommand<Window> CancelCommand
+        {
+            get
+            {
+                var command = new RelayCommand<Window>((window) =>
+                {
+                    window.Close();
+                });
+                return command;
+            }
+        }
     }
 }
