3301cdc60182363254cbbc5a15f2b8aeb5f37aa0
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-05 11:56:04 +0800

    仓库设置

--- StoreManageSystem/StoreManagement/ViewModel/EditStoreViewModel.cs
+++ StoreManageSystem/StoreManagement/ViewModel/EditStoreViewModel.cs
@@ -1,20 +1,69 @@
 ﻿using GalaSoft.MvvmLight;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+using StoreManagement.Service;
+using System.Windows;
 
 namespace StoreManagement.ViewModel
 {
     public class EditStoreViewModel:ViewModelBase
     {
+        public EditStoreViewModel()
+        {
+                
+        }
 
         private Store store = new Store();
         public Store Store
         {
             get { return store; }
             set { store = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        public RelayCommand<Window> EditCommand
+        {
+            get
+            {
+                var command = new RelayCommand<Window>((window) =>
+                {
+                    if (string.IsNullOrEmpty(Store.Name) == true                    )
+                    {
+                        MessageBox.Show("仓库名称不能为空");
+                        return;
+                    }
+
+                    var service = new StoreService();
+                    int count = service.Update(Store);
+                    if (count > 0)
+                    {
+                        MessageBox.Show("修改成功");
+                        // 关闭当前窗体
+                        window.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("修改失败");
+                    }
+                });
+                return command;
+            }
+        }
+
+        /// <summary>
+        /// 取消
+        /// </summary>
+        public RelayCommand<Window> CancelCommand
+        {
+            get
+            {
+                var command = new RelayCommand<Window>((window) =>
+                {
+                    window.Close();
+                });
+                return command;
+            }
         }
     }
 }
